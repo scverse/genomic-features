50f6369373b3a57fc96112a22d6ebeaa61adbf3d
Isaac Virshup <ivirshup@gmail.com>
2023-04-24 16:26:38 +0200

    First features (#1)
    * Add dataset download + basic class
    
    * Try cache-ing annotations
    
    * Add sqlite dependency
    
    * Api doc page
    
    * Basic usage notebook

---
+++ src/genomic_annotations/ensembl/ensembldb.py
@@ -0,0 +1,51 @@
+from __future__ import annotations
+
+import ibis
+
+from genomic_annotations._core.cache import retrieve_annotation
+
+BIOC_ANNOTATION_HUB_URL = (
+    "https://bioconductorhubs.blob.core.windows.net/annotationhub/"
+)
+ENSEMBL_URL_TEMPLATE = (
+    BIOC_ANNOTATION_HUB_URL + "AHEnsDbs/v{version}/EnsDb.{species}.v{version}.sqlite"
+)
+
+
+def annotation(species: str, version: str | int):
+    """Get an annotation database for a species and version.
+
+    Parameters
+    ----------
+    species
+        The species name. E.g. Hsapiens for human, Mmusculus for mouse.
+    version
+        The ensembl release number.
+
+    Returns
+    -------
+    EnsemblDB
+        The annotation database.
+    """
+    return EnsemblDB(
+        ibis.sqlite.connect(
+            retrieve_annotation(
+                ENSEMBL_URL_TEMPLATE.format(species=species, version=version)
+            )
+        )
+    )
+
+
+class EnsemblDB:
+    """Ensembl annotation database."""
+
+    def __init__(self, connection: ibis.BaseBackend):
+        self.db = connection
+
+    def genes(self):
+        """Get the genes table."""
+        return self.db.table("gene").execute()
+
+    def chromosomes(self):
+        """Get chromosome information."""
+        return self.db.table("chromosome").execute()
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,12 +1,13 @@
-import pytest
+import pandas as pd
 
-import genomic_annotations
+import genomic_annotations as ga
 
 
 def test_package_has_version():
-    assert genomic_annotations.__version__ is not None
+    assert ga.__version__ is not None
 
 
-@pytest.mark.skip(reason="This decorator should be removed when test passes.")
-def test_example():
-    assert 1 == 0  # This test is designed to fail.
+def test_genes():
+    genes = ga.ensembl.annotation("Hsapiens", 108).genes()
+
+    assert isinstance(genes, pd.DataFrame)
