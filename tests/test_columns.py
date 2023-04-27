9da026575e90d38e397736c2cf7d6ff887e3f704
Laura <laura.d.martens@icloud.com>
2023-04-27 19:19:18 +0200

    Fix tests

--- tests/test_columns.py
+++ tests/test_columns.py
@@ -13,31 +13,31 @@ def test_columns_subset(hsapiens108):
     assert set(result.columns) == {"gene_id", "gene_name"}
 
 
-def test_listColumns(hsapiens108):
-    result = hsapiens108.listColumns("gene")
+def test_list_columns(hsapiens108):
+    result = hsapiens108.list_columns("gene")
     assert len(result) == len(hsapiens108.genes().columns)
     assert hsapiens108.genes().columns.isin(result).all()
 
 
-def test_cleanColumns(hsapiens108):
-    result = hsapiens108.cleanColumns(["gene_id", "gene_name"])
+def test_clean_columns(hsapiens108):
+    result = hsapiens108.clean_columns(["gene_id", "gene_name"])
     assert result == ["gene_id", "gene_name"]
-    result = hsapiens108.cleanColumns(["gene_id", "invalid_column"])
+    result = hsapiens108.clean_columns(["gene_id", "invalid_column"])
     assert result == ["gene_id"]
     with pytest.raises(ValueError):
-        hsapiens108.cleanColumns(["invalid_column"])
+        hsapiens108.clean_columns(["invalid_column"])
     with pytest.raises(ValueError):
-        hsapiens108.cleanColumns([])
+        hsapiens108.clean_columns([])
 
 
-def test_tablesForColumns(hsapiens108):
-    result = hsapiens108.tablesForColumns(["gene_id", "gene_name"])
+def test_tables_for_columns(hsapiens108):
+    result = hsapiens108.tables_for_columns(["gene_id", "gene_name"], start_with="gene")
     assert result == ["gene"]
-    result = hsapiens108.tablesForColumns(["gene_id", "transcript_id"])
-    assert result == ["entrezgene", "gene", "tx"]
-    result = hsapiens108.tablesForColumns(["gene_name", "invalid_column"])
+    result = hsapiens108.tables_for_columns(["gene_id", "transcript_id"])
+    assert result == ["entrezgene"]
+    result = hsapiens108.tables_for_columns(["gene_name", "invalid_column"])
     assert result == ["gene"]
     with pytest.raises(ValueError):
-        hsapiens108.tablesForColumns(["invalid_column"])
+        hsapiens108.tables_for_columns(["invalid_column"])
     with pytest.raises(ValueError):
-        hsapiens108.tablesForColumns([])
+        hsapiens108.tables_for_columns([])
