7b6e8ac59a3d875fa92d0b24da08ee9bbffc1016
Laura <laura.d.martens@icloud.com>
2023-04-28 12:57:16 +0200

    Add chained joins functionality

--- src/genomic_features/ensembl/ensembldb.py
+++ src/genomic_features/ensembl/ensembldb.py
@@ -49,17 +49,28 @@ class EnsemblDB:
         self,
         cols: list = None,
         filter: filters.AbstractFilterExpr = filters.EmptyFilter(),
+        join_type: str = "inner",
     ) -> pd.DataFrame:
         """Get the genes table."""
 
+        return_cols = cols
         table = "gene"
         if cols is None:
             # TODO: check why R adds entrezid
             cols = self.list_columns(table)  # get all columns
 
-        query = self.build_query(table, cols, filter)
+        cols = self.clean_columns(cols)
+        filter.required_tables()
+        cols = list(set(cols) | filter.columns())  # add columns from filter
 
-        return query.execute()
+        query = self.build_query(table, cols, filter, join_type)
+
+        result = query.execute()
+
+        # order columns
+        return_cols = return_cols + [col for col in cols if col not in return_cols]
+        result = result[return_cols]
+        return result
 
     def chromosomes(self):
         """Get chromosome information."""
@@ -69,9 +80,6 @@ class EnsemblDB:
         """
         Build a query for the genomic features table.
         """
-        cols = self.clean_columns(cols)
-        filter.required_tables()
-        cols = list(set(cols) | filter.columns())  # add columns from filter
 
         # check if join is required
         if len(self.tables_for_columns(cols)) > 1:
@@ -88,19 +96,83 @@ class EnsemblDB:
         """
         # check tables for join
         tables = self.tables_for_columns(cols, start_with=start_with)
-        if start_with not in tables:  # check if start_with is a valid table
-            raise ValueError(f"Invalid table: {start_with}")
-        tables.remove(start_with)  # remove start_with from tables
         # TODO: check what ordering by degree does
 
+        if join_type == "inner":
+            query = self.inner_join_query(tables)
+        elif join_type == "left":
+            query = self.left_join_query(tables)
+        else:
+            raise ValueError(f"Invalid join type: {join_type}")
+
+        return query
+
+    def inner_join_query(self, tables):
+        """
+        Join tables in inner join and return a query.
+        """
         # build query
-        query = self.db.table(start_with)
-        for t in tables:
+        query = self.db.table(tables[0])
+        for t in tables[1:]:
             # determine common columns
             # TODO: Determine if we want to use all common columns
             common_cols = list(set(query.columns) & set(self.db.table(t).columns))
-            query = query.join(self.db.table(t), predicates=common_cols, how=join_type)
+            query = query.join(self.db.table(t), predicates=common_cols, how="inner")
         return query
+
+    def left_join_query(self, query, tables):
+        """
+        Join tables in left join and return a query.
+        """
+        for t in tables:
+            # determine common columns
+            common_cols = list(set(query.columns) & set(self.db.table(t).columns))
+            query = query.join(
+                self.db.table(t),
+                predicates=common_cols,
+                how="left",
+                suffixes=("", "_y"),
+            )
+            query = query.drop(
+                *[f"{c}_y" for c in common_cols]
+            )  # drop duplicate columns
+        return query
+
+    def list_tables(self) -> list:
+        """
+        List all tables available in the genomic features database.
+        """
+        return self.db.list_tables()
+
+    def tables_by_degree(self, tab: list[str] = None) -> list:
+        """
+        Order tables available in the genomic features database.
+        """
+        if tab is None:
+            tab = self.list_tables()  # list of table names
+        # check that all tables are in the database and print warning
+        if not set(tab).issubset(set(self.list_tables())):
+            print(
+                f"Warning: tables {set(tab) - set(self.list_tables())} are not in the database."
+            )
+            tab = list(set(tab) & set(self.list_tables()))  # remove tables not in db
+
+        # order tables
+
+        table_order = {
+            "gene": 1,
+            "tx": 2,
+            "tx2exon": 3,
+            "exon": 4,
+            "chromosome": 5,
+            "protein": 6,
+            "uniprot": 7,
+            "protein_domain": 8,
+            "entrezgene": 9,
+            "metadata": 99,
+        }
+
+        return sorted(tab, key=lambda x: table_order[x])
 
     def list_columns(self, table=None) -> list:
         """
@@ -137,7 +209,7 @@ class EnsemblDB:
         Return a list of tables that contain the specified columns.
         """
         cols = self.clean_columns(cols)
-        table_list = self.db.list_tables()
+        table_list = self.tables_by_degree()  # list of table names
 
         # remove start_with from table_list and add it to the beginning of the list
         if start_with is not None:
--- tests/test_columns.py
+++ tests/test_columns.py
@@ -8,9 +8,24 @@ def hsapiens108():
     return gf.ensembl.annotation("Hsapiens", 108)
 
 
-def test_columns_subset(hsapiens108):
-    result = hsapiens108.genes(cols=["gene_id", "gene_name"])
-    assert set(result.columns) == {"gene_id", "gene_name"}
+def test_tables_by_degree(hsapiens108):
+    result = hsapiens108.tables_by_degree()
+    assert result == [
+        "gene",
+        "tx",
+        "tx2exon",
+        "exon",
+        "chromosome",
+        "protein",
+        "uniprot",
+        "protein_domain",
+        "entrezgene",
+        "metadata",
+    ]
+    result = hsapiens108.tables_by_degree(tab=["protein", "exon"])
+    assert result == ["exon", "protein"]
+    result = hsapiens108.tables_by_degree(tab=["protein", "invalid_table"])
+    assert result == ["protein"]
 
 
 def test_list_columns(hsapiens108):
@@ -33,11 +48,58 @@ def test_clean_columns(hsapiens108):
 def test_tables_for_columns(hsapiens108):
     result = hsapiens108.tables_for_columns(["gene_id", "gene_name"], start_with="gene")
     assert result == ["gene"]
-    result = hsapiens108.tables_for_columns(["gene_id", "transcript_id"])
-    assert result == ["entrezgene"]
+    result = hsapiens108.tables_for_columns(["gene_id", "tx_id"])
+    assert result == ["gene", "tx"]
     result = hsapiens108.tables_for_columns(["gene_name", "invalid_column"])
     assert result == ["gene"]
     with pytest.raises(ValueError):
         hsapiens108.tables_for_columns(["invalid_column"])
     with pytest.raises(ValueError):
         hsapiens108.tables_for_columns([])
+
+
+# Test simple subsetting to columns in one table gene
+def test_simple_subsetting(hsapiens108):
+    result = hsapiens108.genes(cols=["gene_id", "gene_name"])
+    assert result.shape == (70616, 2)
+    assert result.columns.tolist() == ["gene_id", "gene_name"]
+
+
+# Test subsetting to columns in multiple tables
+def test_multiple_table_subsetting(hsapiens108):
+    # table genes and transcripts
+    result = hsapiens108.genes(
+        cols=["gene_id", "gene_name", "tx_id"],
+        join_type="inner",
+    )
+    assert result.shape == (275721, 3)
+    assert list(result.columns) == ["gene_id", "gene_name", "tx_id"]
+
+    # table genes and transcripts with filter
+    result = hsapiens108.genes(
+        cols=["gene_id", "gene_name", "tx_id"],
+        join_type="inner",
+        filter=gf.filters.GeneBioTypeFilter(["protein_coding"]),
+    )
+    assert result.shape == (188553, 4)
+    assert list(result.columns) == ["gene_id", "gene_name", "tx_id", "gene_biotype"]
+
+    # table genes, transcripts and exons and filter
+
+    result = hsapiens108.genes(
+        cols=["gene_id", "gene_name", "tx_id", "exon_id"],
+        join_type="inner",
+        filter=gf.filters.GeneIDFilter(["ENSG00000139618"]),
+    )
+
+    assert result.shape == (186, 4)
+    assert list(result.columns) == ["gene_id", "gene_name", "tx_id", "exon_id"]
+
+    # test left join
+    # table genes and transcripts
+    result = hsapiens108.genes(
+        cols=["gene_id", "gene_name", "tx_id"],
+        join_type="inner",
+    )
+    assert result.shape == (275721, 3)
+    assert list(result.columns) == ["gene_id", "gene_name", "protein_id"]
